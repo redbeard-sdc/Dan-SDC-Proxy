ee8db8be89b67c851bde73b6a648f6a0a770e936
Dan Carbonell <dan1525@gmail.com>
2019-05-01 19:11:13 -0600

    proxy set up with AWS links, working on local host

--- proxy/server.js
+++ proxy/server.js
@@ -1,40 +1,37 @@
-require('newrelic');
-const express = require('express');
+const nr = require('newrelic');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
-const cors = require('cors');
-// const httpProxy = require('http-proxy');
+const cors = require("cors");
+const httpProxy = require("http-proxy");
 
-// const proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({});
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.all('/api/*', (req, res) => {
-//   proxy.web(req, res, {
-//     target: 'http://ec2-18-222-0-232.us-east-2.compute.amazonaws.com/'
-//   });
-// });
+// Dmitri Reviews Service
+app.all("/api/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-234-162-20.compute-1.amazonaws.com/"
+  });
+});
 
-// app.all('/locations/*', (req, res) => {});
+// Bevan Location Service
+app.all("/location/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://loadballoc-231b959af5018acb.elb.us-east-2.amazonaws.com/"
+  });
+});
 
-// app.all('/prices/*', (req, res) => {
-//   proxy.web(req, res, {
-//     target: 'http://ec2-18-222-174-176.us-east-2.compute.amazonaws.com/'
-//   });
-// });
+// Dan Booking Service
+app.all("/prices/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-3-17-179-228.us-east-2.compute.amazonaws.com/"
+  });
+});
 
-// app.all('/hotels/*', (req, res) => {
-//   proxy.web(req, res, {
-//     target: 'http://ec2-18-188-161-215.us-east-2.compute.amazonaws.com/'
-//   });
-// });
-
-// app.all('/header/*', (req, res) => {
-//   proxy.web(req, res, {
-//     target: 'http://ec2-3-17-179-159.us-east-2.compute.amazonaws.com/'
-//   });
-// });
-
-app.listen(port, () => console.log(`Proxy server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
